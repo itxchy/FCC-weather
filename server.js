95456d9f9a81eab0e73b2903682b61caca595106
Matt Trifilo <matt.trifilo@gmail.com>
2016-06-17 18:15:05 -0500

    fix spacing

--- server.js
+++ server.js
@@ -10,20 +10,20 @@ const openWeatherApiKey = process.env.OPEN_WEATHER_API_KEY;
 
 function externalRequest (uri, res, errorMessage) {
 
-	request(uri, (err, apiResponse, body) => {
+    request(uri, (err, apiResponse, body) => {
 
-		if (!err && apiResponse.statusCode == 200) {
-			res.send(body);
-		}
+        if (!err && apiResponse.statusCode == 200) {
+            res.send(body);
+        }
 
-		if (err) {
-			res.status(500).end('Error!', err);
-		}
+        if (err) {
+            res.status(500).end('Error!', err);
+        }
 
-		else {
-			res.end(errorMessage, apiResponse.statusCode);
-		}
-	});
+        else {
+            res.end(errorMessage, apiResponse.statusCode);
+        }
+    });
 }
 
 app.use(compression());
@@ -31,72 +31,72 @@ app.use(compression());
 app.use('/', express.static(__dirname + '/public'));
 
 app.get('/api/location/geocoords', (req, res) => {
-	let lat = req.param('lat');
-	let long = req.param('long');
-	let googleGeoUri = `http://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}`;
-	let errorMessage = 'ERROR: Google Maps API request for location from Geo Coords returned status code: ';
+    let lat = req.param('lat');
+    let long = req.param('long');
+    let googleGeoUri = `http://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}`;
+    let errorMessage = 'ERROR: Google Maps API request for location from Geo Coords returned status code: ';
 
-	externalRequest(googleGeoUri, res, errorMessage);
+    externalRequest(googleGeoUri, res, errorMessage);
 
 });
 
 app.get('/api/location/zipcode', (req, res) => {
-	let zipcode = req.param('zipcode');
-	let googleZipcodeUri = `http://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}`;
-	let errorMessage = 'ERROR: Google Maps API request for location from zipcode returned status code: ';
+    let zipcode = req.param('zipcode');
+    let googleZipcodeUri = `http://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}`;
+    let errorMessage = 'ERROR: Google Maps API request for location from zipcode returned status code: ';
 
-	externalRequest(googleZipcodeUri, res, errorMessage);
+    externalRequest(googleZipcodeUri, res, errorMessage);
 
 });
 
 app.get('/api/location/geolocation', (req, res) => {
-	let lat = req.param('lat');
-	let long = req.param('long');
-	let googleGeoUri = `http://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}`;
-	let errorMessage = 'ERROR: Google Maps API request for loction for geo coordinates returned status code: ';
+    let lat = req.param('lat');
+    let long = req.param('long');
+    let googleGeoUri = `http://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}`;
+    let errorMessage = 'ERROR: Google Maps API request for loction for geo coordinates returned status code: ';
 
-	externalRequest(googleGeoUri, res, errorMessage);
-	
+    externalRequest(googleGeoUri, res, errorMessage);
+    
 });
 
 app.get('/api/weather/geocoords', (req, res) => {
-	let lat = req.param('lat');
-	let long = req.param('long');
-	let geoUri = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=${openWeatherApiKey}`;
-	let errorMessage = 'ERROR: Open Weather Map API request for current conditions from Geo Coords returned status code: ';
+    let lat = req.param('lat');
+    let long = req.param('long');
+    let geoUri = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=${openWeatherApiKey}`;
+    let errorMessage = 'ERROR: Open Weather Map API request for current conditions from Geo Coords returned status code: ';
 
-	externalRequest(geoUri, res, errorMessage);
+    externalRequest(geoUri, res, errorMessage);
 
 });
 
 app.get('/api/weather/zipcode', (req, res) => {
-	let zipcode = req.param('zipcode');
-	let zipcodeUri = `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&units=imperial&APPID=${openWeatherApiKey}`;
-	let errorMessage = 'ERROR: Open Weather Map API request for current conditions from Zipcode returned status code: ';
+    let zipcode = req.param('zipcode');
+    let zipcodeUri = `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&units=imperial&APPID=${openWeatherApiKey}`;
+    let errorMessage = 'ERROR: Open Weather Map API request for current conditions from Zipcode returned status code: ';
 
-	externalRequest(zipcodeUri, res, errorMessage);
+    externalRequest(zipcodeUri, res, errorMessage);
 
 }); 
 
 app.get('/api/forecast/geocoords', (req, res) => {
-	let lat = req.param('lat');
-	let long = req.param('long');
-	let geoForecastUri = `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&cnt=7&units=imperial&APPID=${openWeatherApiKey}`;
-	let errorMessage = 'ERROR: Open Weather Forecast call with geo coords returned status code: ';
+    let lat = req.param('lat');
+    let long = req.param('long');
+    let geoForecastUri = `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&cnt=7&units=imperial&APPID=${openWeatherApiKey}`;
+    let errorMessage = 'ERROR: Open Weather Forecast call with geo coords returned status code: ';
 
-	externalRequest(geoForecastUri, res, errorMessage);
+    externalRequest(geoForecastUri, res, errorMessage);
 
 });
 
 app.get('/api/forecast/zipcode', (req, res) => {
-	let zipcode = req.param('zipcode');
-	let zipcodeForecastUri = `http://api.openweathermap.org/data/2.5/forecast/daily?zip=${zipcode}&cnt=7&units=imperial&APPID=${openWeatherApiKey}`;
-	let errorMessage = 'ERROR: Open Weather Forecast call with zipcode returned status code: ';
+    let zipcode = req.param('zipcode');
+    let zipcodeForecastUri = `http://api.openweathermap.org/data/2.5/forecast/daily?zip=${zipcode}&cnt=7&units=imperial&APPID=${openWeatherApiKey}`;
+    let errorMessage = 'ERROR: Open Weather Forecast call with zipcode returned status code: ';
 
-	externalRequest(zipcodeForecastUri, res, errorMessage);
+    externalRequest(zipcodeForecastUri, res, errorMessage);
 
 });
 
 app.listen(4000, () => {
-	console.log('Express app listening on port 4000!');
+    console.log('Express app listening on port 4000!');
 });
--- source/js/app.js
+++ source/js/app.js
@@ -6,302 +6,302 @@ var weatherApp = angular.module('weatherApp', []);
 
 weatherApp.factory('location', function($q, $http) {
 
-	var clientOrigin = window.location.origin;
-	var lat = null;
-	var long = null;
-	var zipcode = null;
+    var clientOrigin = window.location.origin;
+    var lat = null;
+    var long = null;
+    var zipcode = null;
 
-	return {
+    return {
 
-		getGeoCoords: function() {
+        getGeoCoords: function() {
 
-			return $q(function(resolve, reject) {
+            return $q(function(resolve, reject) {
 
-				if(navigator.geolocation) {
+                if(navigator.geolocation) {
 
-					// if successful, resolve the promise with the position object
-					navigator.geolocation.getCurrentPosition(function(position) {
-						return resolve(position);
-					}, 
+                    // if successful, resolve the promise with the position object
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                        return resolve(position);
+                    }, 
 
-					// if there's an error, reject the promise and pass the error along
-					function (error) {
-						return reject(error);
-					});
-				} 
+                    // if there's an error, reject the promise and pass the error along
+                    function (error) {
+                        return reject(error);
+                    });
+                } 
 
-				else {
-					return reject();
-				}
-			});
-		},
+                else {
+                    return reject();
+                }
+            });
+        },
 
-		updateZipcode: function(zipcode) {
-			this.zipcode = zipcode;
-			return this.zipcode;
-		},
+        updateZipcode: function(zipcode) {
+            this.zipcode = zipcode;
+            return this.zipcode;
+        },
 
-		/******* Calls Google Map's API, returns location details based on geo coordinates *******/
-		getGeoLocationName: function(lat, long) {
+        /******* Calls Google Map's API, returns location details based on geo coordinates *******/
+        getGeoLocationName: function(lat, long) {
 
-			return $http.get(`${clientOrigin}/api/location/geolocation?lat=${lat}&long=${long}`)
-				.then(function(locationData) {
+            return $http.get(`${clientOrigin}/api/location/geolocation?lat=${lat}&long=${long}`)
+                .then(function(locationData) {
 
-					if (angular.isObject(locationData)) {
-						return locationData;
-					} 
+                    if (angular.isObject(locationData)) {
+                        return locationData;
+                    } 
 
-					else {
-						return $q.reject(locationData);
-					}
-				});
-		},
+                    else {
+                        return $q.reject(locationData);
+                    }
+                });
+        },
 
-		getZipcodeLocationName: function(zipcode) {
+        getZipcodeLocationName: function(zipcode) {
 
-			return $http.get(`${clientOrigin}/api/location/zipcode?zipcode=${zipcode}`)
-				.then(function(locationData) {
+            return $http.get(`${clientOrigin}/api/location/zipcode?zipcode=${zipcode}`)
+                .then(function(locationData) {
 
-					if (angular.isObject(locationData)) {
-						return locationData;
-					} 
+                    if (angular.isObject(locationData)) {
+                        return locationData;
+                    } 
 
-					else {
-						return $q.reject(locationData);
-					}
-				});
-		},
+                    else {
+                        return $q.reject(locationData);
+                    }
+                });
+        },
 
-		zipcode: zipcode,
-		lat: lat,
-		long: long
-	};
+        zipcode: zipcode,
+        lat: lat,
+        long: long
+    };
 });
 
 weatherApp.factory('weatherFactory', function($http, $q) {
-	var clientOrigin = window.location.origin;
-	var weatherObject = null;
+    var clientOrigin = window.location.origin;
+    var weatherObject = null;
 
-	return {
-		/******* Calls OpenWeatherMap's API, returns current weather data *******/
-		getWeatherFromGeoCoords: function(lat, long) {
+    return {
+        /******* Calls OpenWeatherMap's API, returns current weather data *******/
+        getWeatherFromGeoCoords: function(lat, long) {
 
-			return $http.get(`${clientOrigin}/api/weather/geocoords?lat=${lat}&long=${long}`)
-				.then(function(weatherData) {
+            return $http.get(`${clientOrigin}/api/weather/geocoords?lat=${lat}&long=${long}`)
+                .then(function(weatherData) {
 
-					if (angular.isObject(weatherData)) {
-						return weatherData;
-					} 
+                    if (angular.isObject(weatherData)) {
+                        return weatherData;
+                    } 
 
-					else {
-						return $q.reject(weatherData);
-					}
-				});
-		},
+                    else {
+                        return $q.reject(weatherData);
+                    }
+                });
+        },
 
-		getWeatherFromZipcode: function(zipcode) {
+        getWeatherFromZipcode: function(zipcode) {
 
-			return $http.get(`${clientOrigin}/api/weather/zipcode?zipcode=${zipcode}`)
-				.then(function(weatherData) {
+            return $http.get(`${clientOrigin}/api/weather/zipcode?zipcode=${zipcode}`)
+                .then(function(weatherData) {
 
-					if (angular.isObject(weatherData)) {
-						return weatherData;
-					} 
+                    if (angular.isObject(weatherData)) {
+                        return weatherData;
+                    } 
 
-					else {
-						return $q.reject(weatherData);
-					}
-				});
-		},
+                    else {
+                        return $q.reject(weatherData);
+                    }
+                });
+        },
 
-		/******* Calls OpenWeatherMap's API, returns 7 day forcast *******/   
-		getForecastFromGeoCoords: function(lat, long) {
+        /******* Calls OpenWeatherMap's API, returns 7 day forcast *******/   
+        getForecastFromGeoCoords: function(lat, long) {
 
-			return $http.get(`${clientOrigin}/api/forecast/geocoords?lat=${lat}&long=${long}`)
-				.then(function(forecastData) {
+            return $http.get(`${clientOrigin}/api/forecast/geocoords?lat=${lat}&long=${long}`)
+                .then(function(forecastData) {
 
-					if (angular.isObject(forecastData)) {
-						return forecastData;
-					} 
+                    if (angular.isObject(forecastData)) {
+                        return forecastData;
+                    } 
 
-					else {
-						return $q.reject(forecastData);
-					}
-				});
-		},
+                    else {
+                        return $q.reject(forecastData);
+                    }
+                });
+        },
 
-		getForecastFromZipcode: function(zipcode) {
+        getForecastFromZipcode: function(zipcode) {
 
-			return $http.get(`${clientOrigin}/api/forecast/zipcode?zipcode=${zipcode}`)
-				.then(function(forecastData) {
+            return $http.get(`${clientOrigin}/api/forecast/zipcode?zipcode=${zipcode}`)
+                .then(function(forecastData) {
 
-					if (angular.isObject(forecastData)) {
-						return forecastData;
-					}
+                    if (angular.isObject(forecastData)) {
+                        return forecastData;
+                    }
 
-					else {
-						return $q.reject(forecastData);
-					}
-				});
-		},
+                    else {
+                        return $q.reject(forecastData);
+                    }
+                });
+        },
 
-		initializeWeatherIcons: function(weatherId) {
+        initializeWeatherIcons: function(weatherId) {
 
-			/******* WX Icons *******/
-			var icon = "wi-na";
+            /******* WX Icons *******/
+            var icon = "wi-na";
 
-			var currentID = weatherId;
+            var currentID = weatherId;
 
-			// Thunderstorm
-			if ( currentID >= 200 && currentID <= 232 ) {
-				icon = 'wi-day-thunderstorm';
-			}
+            // Thunderstorm
+            if ( currentID >= 200 && currentID <= 232 ) {
+                icon = 'wi-day-thunderstorm';
+            }
 
-			// Drizzle 
-			if ( currentID >= 300 && currentID <= 321 ) {
-				icon = 'wi-day-showers';
-			}
+            // Drizzle 
+            if ( currentID >= 300 && currentID <= 321 ) {
+                icon = 'wi-day-showers';
+            }
 
-			// Rain
-			if ( currentID >= 500 && currentID <= 531 ) {
-				icon = 'wi-day-rain';
-			}
+            // Rain
+            if ( currentID >= 500 && currentID <= 531 ) {
+                icon = 'wi-day-rain';
+            }
 
-			// Snow
-			if ( currentID >= 600 && currentID <= 622 ) {
-				icon = 'wi-day-snow';
-			}
+            // Snow
+            if ( currentID >= 600 && currentID <= 622 ) {
+                icon = 'wi-day-snow';
+            }
 
-			// Atmosphere (fog, haze, etc.) 
-			if ( currentID >= 701 && currentID <= 781 ) {
-				icon = 'wi-day-fog';
-			}
+            // Atmosphere (fog, haze, etc.) 
+            if ( currentID >= 701 && currentID <= 781 ) {
+                icon = 'wi-day-fog';
+            }
 
-			// Sun
-			if ( currentID === 800 || currentID === 801 ) {
-				icon = 'wi-day-sunny';
-			}
+            // Sun
+            if ( currentID === 800 || currentID === 801 ) {
+                icon = 'wi-day-sunny';
+            }
 
-			// Clouds
-			if ( currentID >= 802 && currentID <= 804 ) {
-				icon = 'wi-day-cloudy';
-			}
+            // Clouds
+            if ( currentID >= 802 && currentID <= 804 ) {
+                icon = 'wi-day-cloudy';
+            }
 
-			return icon;
-		}
-	};
+            return icon;
+        }
+    };
 });
 
 weatherApp.controller('zipcode', ['$scope', '$rootScope', 'location', function($scope, $rootScope, location) {
 
-	$scope.zipcodeClickHandler = function() {
+    $scope.zipcodeClickHandler = function() {
 
-		location.updateZipcode($scope.zipcode);
+        location.updateZipcode($scope.zipcode);
 
-		// fires a 'zipcode received' event with this $scope's zipcode as as arg
-		$rootScope.$broadcast('zipcodeUpdate', $scope.zipcode);
-	};
+        // fires a 'zipcode received' event with this $scope's zipcode as as arg
+        $rootScope.$broadcast('zipcodeUpdate', $scope.zipcode);
+    };
 }]);
 
 weatherApp.controller('weatherCtrl', function ($scope, weatherFactory, location, $q) {
 
-	$scope.hideDataFromView = true;
+    $scope.hideDataFromView = true;
 
-	function getGeoWeatherAndLocation() {
+    function getGeoWeatherAndLocation() {
 
-		return $q(function (resolve, reject) {
+        return $q(function (resolve, reject) {
 
-			weatherFactory.getWeatherFromGeoCoords(location.lat, location.long)
-			.then(function (data) {
+            weatherFactory.getWeatherFromGeoCoords(location.lat, location.long)
+            .then(function (data) {
 
-				$scope.weather = data.data;
-				var weatherId = $scope.weather.weather[0].id;
-				$scope.icon = weatherFactory.initializeWeatherIcons(weatherId);
+                $scope.weather = data.data;
+                var weatherId = $scope.weather.weather[0].id;
+                $scope.icon = weatherFactory.initializeWeatherIcons(weatherId);
 
-				if (!data) {
-					reject();
-				}
+                if (!data) {
+                    reject();
+                }
 
-				location.getGeoLocationName(location.lat, location.long)
-				.then(function(data) {
+                location.getGeoLocationName(location.lat, location.long)
+                .then(function(data) {
 
-					$scope.location = data.data.results[5].address_components[0].long_name;
-					weatherFactory.getForecastFromGeoCoords(location.lat, location.long)
-					.then(function(data) {
+                    $scope.location = data.data.results[5].address_components[0].long_name;
+                    weatherFactory.getForecastFromGeoCoords(location.lat, location.long)
+                    .then(function(data) {
 
-						$scope.forecast = data.data;
-						resolve(data);
-					});
-				});
-			});
-		});
-	}
+                        $scope.forecast = data.data;
+                        resolve(data);
+                    });
+                });
+            });
+        });
+    }
 
-	location.getGeoCoords()
-	.then(function(position) {
+    location.getGeoCoords()
+    .then(function(position) {
 
-		location.lat = position.coords.latitude;
-		location.long = position.coords.longitude;
+        location.lat = position.coords.latitude;
+        location.long = position.coords.longitude;
 
-		getGeoWeatherAndLocation()
-		.then(function() {
+        getGeoWeatherAndLocation()
+        .then(function() {
 
-			$scope.hideDataFromView = false;
-		}, 
+            $scope.hideDataFromView = false;
+        }, 
 
-		function(error) {
-		//error, show zipcode instead
-		});
+        function(error) {
+        //error, show zipcode instead
+        });
 
-		}, function(error) {
-			return error;
-	});
+        }, function(error) {
+            return error;
+    });
 
-	// function for the event listener
-	// when a 'zipcode received' event appears, 
-	// this function fires.
-	function getZipcodeWeatherAndLocation(zipcode) {
+    // function for the event listener
+    // when a 'zipcode received' event appears, 
+    // this function fires.
+    function getZipcodeWeatherAndLocation(zipcode) {
 
-		return $q(function (resolve, reject) {
+        return $q(function (resolve, reject) {
 
-			weatherFactory.getWeatherFromZipcode(zipcode)
-			.then(function (data) {
+            weatherFactory.getWeatherFromZipcode(zipcode)
+            .then(function (data) {
 
-				$scope.weather = data.data;
-				var weatherId = $scope.weather.weather[0].id;
-				$scope.icon = weatherFactory.initializeWeatherIcons(weatherId);
+                $scope.weather = data.data;
+                var weatherId = $scope.weather.weather[0].id;
+                $scope.icon = weatherFactory.initializeWeatherIcons(weatherId);
 
-				if (!data) {
-					reject(error);
-				}
+                if (!data) {
+                    reject(error);
+                }
 
-				location.getZipcodeLocationName(zipcode)
-				.then(function(data) {
+                location.getZipcodeLocationName(zipcode)
+                .then(function(data) {
 
-					console.log('zipcode location data', data);
-					$scope.location = data.data.results[0].address_components[1].long_name;
+                    console.log('zipcode location data', data);
+                    $scope.location = data.data.results[0].address_components[1].long_name;
 
-					weatherFactory.getForecastFromZipcode(zipcode)
-					.then(function(data) {
-						console.log('zipcode forecast data: ', data.data);
-						$scope.forecast = data.data;
-						resolve(data);
+                    weatherFactory.getForecastFromZipcode(zipcode)
+                    .then(function(data) {
+                        console.log('zipcode forecast data: ', data.data);
+                        $scope.forecast = data.data;
+                        resolve(data);
 
-					});
-				});
-			});
-		});
-	}
+                    });
+                });
+            });
+        });
+    }
 
-	$scope.$on('zipcodeUpdate', function(event, zipcode) {
-		
-		getZipcodeWeatherAndLocation(zipcode)
-		.then(function() {
-			$scope.hideDataFromView = false;
-		}, 
-		function(error) {
-		//error, show zipcode instead
-		//console.log("an error occured in getZipcodeWeatherAndLocation, ", error);
-		});
-	});
+    $scope.$on('zipcodeUpdate', function(event, zipcode) {
+        
+        getZipcodeWeatherAndLocation(zipcode)
+        .then(function() {
+            $scope.hideDataFromView = false;
+        }, 
+        function(error) {
+        //error, show zipcode instead
+        //console.log("an error occured in getZipcodeWeatherAndLocation, ", error);
+        });
+    });
 });
